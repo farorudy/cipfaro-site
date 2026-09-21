081ed28a2a23f068985054a1625574d507457f04
farorudy <rudy.faro@gmail.com>
2026-09-20 22:28:51 -0400

    Clarifier le lien vers les formations en ligne

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
           ))}
         </nav>
         <a className="moodle-button" href={SITE.moodle} target="_blank" rel="noopener noreferrer">
-          Se connecter à la plateforme
+          Accéder aux formations en ligne
         </a>
       </div>
     </header>
