9ba172b13535a241d64209f5c57a65ebd0cf107f
farorudy <rudy.faro@gmail.com>
2026-09-24 18:19:02 -0400

    Ajouter le lien court vers les ateliers numériques

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/ateliers',
+        destination: 'https://ateliers-numeriques-cip-faro.rudyfaro.chatgpt.site',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
